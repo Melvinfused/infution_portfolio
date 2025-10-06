71120630929c750f39f78ff0f3521f977702066f
Melvin Francy <thomasfrancymoothedath0@gmail.com>
2025-10-06 11:11:29 +0530

    Typo Fixes

--- src/navbar.jsx
+++ src/navbar.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import './Navbar.css';
+import './navbar.css';
 
 const Navbar = ({ activeSection, setActiveSection }) => {
   const [expanded, setExpanded] = useState(false);
